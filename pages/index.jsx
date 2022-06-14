9454c3ae71d30bf77bed3b0d3b552ac821bc3ca3
jayke770 <jakevelasco40@gmail.com>
2022-06-15 07:08:02 +0800

    init

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import { Navbar } from 'konsta/react'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Master NFT</title>
+      </Head>
+      <Navbar />
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const konstaConfig = require('konsta/config')
+module.exports = konstaConfig({
+  darkMode: 'class',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
